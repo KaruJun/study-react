d5097ea4757def798dc36d9c3d95c68f701124df
karube <onedra0310@icloud.com>
2022-12-29 01:13:13 +0900

    ⚗️ 不要な記述でいつか消しますが、試しにクリックイベントを書いて見ました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,34 @@
 import Head from 'next/head'
+import { useCallback } from 'react'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import { Main } from 'src/components/Main'
 import classes from 'src/styles/Home.module.css'
 
-export default function Home() {
+// const handleClick = (e, foo) => {
+//   console.log(e.target.href)
+//   e.preventDefault()
+//   alert(foo)
+// }
+
+const foo = 1
+
+export default function Home () {
+  const handleClick = useCallback(e => {
+    console.log(e.target.href)
+    e.preventDefault()
+    alert(foo)
+  }, [])
   return (
     <div className={classes.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Main page="index" />
+      <a href='/about' onClick={handleClick}>
+        ボタン
+      </a>
+      <Main page='index' />
       <Footer />
     </div>
   )
